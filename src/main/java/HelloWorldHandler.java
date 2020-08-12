6d78d8a9d7d93df65a97b5ae11fdf70bfb6b028a
Alex Graham <nagraham00@gmail.com>
2020-08-11 17:02:33 -0700

    Initial commit with HelloWorldHandler and cold start test script

---
+++ src/main/java/HelloWorldHandler.java
@@ -0,0 +1,14 @@
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+public class HelloWorldHandler implements RequestHandler<Map<String, String>, String> {
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        logger.log("Hello World!\n");
+        return null;
+    }
+}
